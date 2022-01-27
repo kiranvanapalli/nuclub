de25e361d8b0c2a9094e9606a8d492dcdc2f9d55
Kiran <kiran.vanapalli123@gmail.com>
2022-01-27 09:51:30 +0530

    update js'

--- application/modules/members/views/all_files_js.php
+++ application/modules/members/views/all_files_js.php
@@ -84,11 +84,11 @@
                     let detail = JSON.parse(data);
                     console.log(detail);
                     if (detail['status'] =="success") {
-                        toastr["success"](detail['message']);
+                        toastr["success"]("Member Details Added Successfully!");
                         window.location.href = "<?php echo base_url(); ?>members";
 
                     } else if(detail['status'] == "error") {
-                        toastr["error"](detail['message']);
+                        toastr["error"]("Mobile Number Already Existed");
                         return false;
                     }
 
